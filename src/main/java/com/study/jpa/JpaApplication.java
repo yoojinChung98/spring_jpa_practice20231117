b7a7bfb52b2b71029645f611847eac41aba52ac5
yoojin <yoojin9199@gmail.com>
2023-11-17 16:52:47 +0900

    initial commit

---
+++ src/main/java/com/study/jpa/JpaApplication.java
@@ -0,0 +1,15 @@
+package com.study.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApplication {
+
+	public static void main(String[] args) {
+
+		// 내장 톰캣을 가동시키는 구문.
+		SpringApplication.run(JpaApplication.class, args);
+	}
+
+}
